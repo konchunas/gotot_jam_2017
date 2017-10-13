8e9e081ec18357774b1d331ccba0015e36b14434
Julian <konchunas@gmail.com>
2017-10-13 13:26:51 +0300

    player rigid body

--- game/level/Player.gd
+++ game/level/Player.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 const GRAVITY = 200.0
 const WALK_SPEED = 200
@@ -15,21 +15,24 @@ func _fixed_process(delta):
 		velocity.x = 0
 
 	var motion = velocity * delta
-	motion = move(motion)
+	#motion = move(motion)
 
-	if (is_colliding()):
-		var n = get_collision_normal()
-		motion = n.slide(motion)
-		velocity = n.slide(velocity)
-		move(motion)
-
-		if get_collider():
-			if get_collider().has_method("on_player_collide"):
-				get_collider().on_player_collide(self)
-
+	# if (is_colliding()):
+		# var n = get_collision_normal()
+		# motion = n.slide(motion)
+		# velocity = n.slide(velocity)
+		# move(motion)
+# 
+		# if get_collider():
+			# if get_collider().has_method("on_player_collide"):
+				# get_collider().on_player_collide(self)
 
 func _ready():
 	set_fixed_process(true)
 
 func _die():
 	set_pos(Vector2(10,10))
+
+func _on_body_enter( body ):
+	if body.has_method("on_player_collide"):
+		body.on_player_collide(self)
