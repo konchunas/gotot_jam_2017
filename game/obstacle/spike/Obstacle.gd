38164c86eefc9860a290be2f07e2d63b54782623
skelewir <skelewir@gmail.com>
2017-10-13 17:12:29 +0300

    sample level background

--- game/obstacle/spike/Obstacle.gd
+++ game/obstacle/spike/Obstacle.gd
@@ -1,23 +1,7 @@
 extends StaticBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	set_fixed_process(true)
-	pass
 
-
-func _on_contact( body ):
-	body._die()
-	pass # replace with function body
-
-func _on_body_enter( body ):
-	body._die()
-	pass # replace with function body
-	
 func on_player_collide(player):
 	player._die()
