97aa5d5d01043a0471217f54de37e3b93297e723
Julian <konchunas@gmail.com>
2017-10-13 15:23:50 +0300

    sticky player and lasting velocities

--- game/player/Player.gd
+++ game/player/Player.gd
@@ -6,6 +6,8 @@ const WALK_SPEED = 200
 var velocity = Vector2()
 var initial_gravity_scale
 var initial_pos = Vector2()
+var previousLinearVelocity = Vector2()
+var previousAngularVelocity = 0.0;
 
 func _ready():
 	set_fixed_process(true)
@@ -22,6 +24,8 @@ func _fixed_process(delta):
 func _input(event):
 	if event.is_action_released("ui_select"):
 		set_gravity_scale(initial_gravity_scale)
+		set_linear_velocity(previousLinearVelocity)
+		set_angular_velocity(previousAngularVelocity)
 
 func _die():
 	set_pos(initial_pos)
@@ -29,9 +33,11 @@ func _die():
 func _on_body_enter( body ):
 	if Input.is_action_pressed("ui_select"):
 		if body.is_in_group("ceiling"):
-#			set_linear_velocity(Vector2(0, 0))
+			previousLinearVelocity = get_linear_velocity()
+			set_linear_velocity(Vector2(0, 0))
+			previousAngularVelocity = get_angular_velocity()
 			set_angular_velocity(0)
-			set_gravity_scale(0)
+			set_gravity_scale(0.0)
 		elif body.is_in_group("floor"):
 			apply_impulse(Vector2(0,0), Vector2(0, -700))
 
