634cf3d0c8b3aaabd925cd0c98a905c32ba3f596
Julian <konchunas@gmail.com>
2017-10-13 12:57:13 +0300

    basic collision

--- game/level/Player.gd
+++ game/level/Player.gd
@@ -6,23 +6,30 @@ const WALK_SPEED = 200
 var velocity = Vector2()
 
 func _fixed_process(delta):
-    velocity.y += delta * GRAVITY
-    if (Input.is_action_pressed("ui_left")):
-        velocity.x = - WALK_SPEED
-    elif (Input.is_action_pressed("ui_right")):
-        velocity.x =   WALK_SPEED
-    else:
-        velocity.x = 0
+	velocity.y += delta * GRAVITY
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x = - WALK_SPEED
+	elif (Input.is_action_pressed("ui_right")):
+		velocity.x =   WALK_SPEED
+	else:
+		velocity.x = 0
 
-    var motion = velocity * delta
-    motion = move(motion)
+	var motion = velocity * delta
+	motion = move(motion)
 
-    if (is_colliding()):
-        var n = get_collision_normal()
-        motion = n.slide(motion)
-        velocity = n.slide(velocity)
-        move(motion)
+	if (is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
+
+		if get_collider():
+			if get_collider().has_method("on_player_collide"):
+				get_collider().on_player_collide(self)
 
 
 func _ready():
-    set_fixed_process(true)
+	set_fixed_process(true)
+
+func _die():
+	set_pos(Vector2(10,10))
