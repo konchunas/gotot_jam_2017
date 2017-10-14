3a317dd6bab0ca992cb3535f4390aa050d75d387
Julian <konchunas@gmail.com>
2017-10-14 07:49:54 +0300

    robotic arm animation

--- game/obstacle/robotic_arm/robotic_arm.gd
+++ game/obstacle/robotic_arm/robotic_arm.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _ready():
+	var anim = get_node("animation").get_animation("piston_pos")
+	anim.set_loop(true)
+	get_node("animation").play("piston_pos")
+
 func on_player_collide(player):
 	if player.has_method("_die"):
 		player._die()
